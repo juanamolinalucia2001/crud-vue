a5c13e757acbad83258e34b02eb6f5d82370d7d4
juanamolinalucia2001 <juana2001molina@gmail.com>
2023-01-31 11:27:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app>
+   <v-card
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar dense class="deep-purple" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Crud en Vue</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    
+    </v-card>
+
+     <v-navigation-drawer
+      v-model="drawer" temporary app>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-title>John Leider</v-list-item-title>
+
+
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+         <v-list-item-content>
+            <v-btn color="success" :to="{name:'home'}">Home</v-btn>
+         </v-list-item-content >
+
+         <v-list-item-content>
+            <v-btn color="success" :to="{name:'tareas-crud'}">Lista Tareas</v-btn>
+         </v-list-item-content>
+        
+      </v-list>
+    </v-navigation-drawer>
+
+
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      drawer:true
+    }
+  }
+};
+</script>
---
+++ src/views/Tareas-crud.vue
@@ -0,0 +1,126 @@
+<template>
+<v-app>
+
+
+    <v-container grid-list-xl>
+        <v-layout row wrap>
+
+            <v-flex md6 >
+                <v-card class="mb-3" v-for="(item, index) in listaTareas" :key="index"> 
+                    <v-card-text>
+                       
+                        <v-chip class="ma-2 ml-0" color="pink" label text-color="white">
+                             <v-icon left>mdi-label</v-icon>
+                            {{item.titulo}}
+                        </v-chip>
+                        <p>{{item.descripcion}}</p>
+                        <v-row  align="center" justify="space-around">
+                        <v-btn color="warning" class="ml-0" @click="editar(index)">Editar</v-btn>
+                        <v-btn color="error" @click="eliminarTarea(item.id)">Eliminar</v-btn>
+                        </v-row>
+                    </v-card-text>
+                </v-card>
+
+
+
+            </v-flex>
+
+            <v-flex md6 v-if="formAgregar">
+                <v-card class="mb-3 pa-6">
+                    <v-form @submit.prevent="agregarTarea">
+                        <v-text-field label="Titulo de tarea" v-model="titulo"></v-text-field>
+                        <v-textarea label="Desscripción de tarea" v-model="descripcion"></v-textarea>
+                        <v-btn color="success" block type="submit">Agregar Tarea</v-btn>
+                    </v-form>
+                </v-card>
+
+            </v-flex>
+
+            <v-flex md6 v-if="!formAgregar">
+                <v-card class="mb-3 pa-6">
+                    <v-form @submit.prevent="agregarTareaEditada    ">
+                        <v-text-field label="Titulo de tarea" v-model="titulo"></v-text-field>
+                        <v-textarea label="Desscripción de tarea" v-model="descripcion"></v-textarea>
+                        <v-btn color="warning" block type="submit">Editar Tarea</v-btn>
+                    </v-form>
+                </v-card>
+
+            </v-flex>
+        </v-layout>
+
+             <v-snackbar
+      v-model="snackbar"
+    >
+      {{ mensaje }}
+
+    
+        <v-btn
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Cerrar
+        </v-btn>
+    </v-snackbar>
+    </v-container>
+</v-app>    
+</template>
+<script>
+export default {
+    data(){
+        return{
+            listaTareas:[
+                {id: 1, titulo: 'Titulo tarea #1', descripcion:'El niño salió a pasear por el campo y vio una oveja que parecía estar sola y perdida. La llevó hasta el río para que tomara un poco de agua y después la alcanzó hasta la casa de la vecina que¡ tenía muchas ovejas. La vecina le agradeció porque era su oveja'},
+                {id: 6, titulo: 'Titulo tarea #2', descripcion:'El niño salió a pasear por el campo y vio una oveja que parecía estar sola y perdida. La llevó hasta el río para que tomara un poco de agua y después la alcanzó hasta la casa de la vecina que¡ tenía muchas ovejas. La vecina le agradeció porque era su oveja, que se había perdido el día anterior mientras estaba pastando'},
+                
+            ],
+            titulo:'',
+            descripcion:'',
+            mensaje:'ajnsjknd',
+            snackbar:false,
+            formAgregar:true,
+        }
+    },
+    methods:{
+        agregarTarea(){
+            // console.log(this.titulo, this.descripcion)
+            if(this.titulo === '' || this.descripcion===''){
+                this.snackbar = true
+                this.mensaje='Llena todos los campos'
+            }else{
+                this.listaTareas.push({
+                    id:Date.now(), 
+                    titulo:this.titulo,
+                    descripcion:this.descripcion
+                })
+                this.titulo=''
+                this.descripcion=''
+                this.snackbar = true
+                this.mensaje='Tarea agregada con exito!'
+
+            }
+        },
+        eliminarTarea(id){
+                this.listaTareas = this.listaTareas.filter(e => e.id != id)
+
+            },
+        editar(index){
+            this.formAgregar = false
+            this.titulo = this.listaTareas[index].titulo
+            this.descripcion =this.listaTareas[index].descripcion
+            this.indexTarea= index
+
+        },
+        agregarTareaEditada(){
+            this.listaTareas[this.indexTarea].titulo = this.titulo
+            this.listaTareas[this.indexTarea].descripcion = this.descripcion
+            this.formAgregar = true
+            this.titulo=''
+            this.descripcion=''
+            this.snackbar = true
+            this.mensaje='Tarea Editada con exito!'
+        }
+    }
+}
+</script>
